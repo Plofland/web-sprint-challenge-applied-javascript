533e540bc1497bf9a06d6ceade8e3d9f0e87c1de
Plofland <ploflan@gmail.com>
2020-12-04 17:11:02 -0500

    Giving up trying to get an array to display

--- components/Tabs.js
+++ components/Tabs.js
@@ -15,39 +15,49 @@
 const result = axios.get("https://lambda-times-api.herokuapp.com/topics");
 console.log(result); //WORKS!!! Returns a promise
 
-console.log(Object.keys(result));//returns an empty array
 
-//Use this for now
-const topcisArray = ["javascript","bootstrap","technology","jquery","node.js"];
 
-console.log(topcisArray);
+// ***GOAL: Get the array with relevent data to display, if can do that, then I can access that data for the rest of task in this file*** 
+// ***TEMPLATE: (code); //(logic behind the attempt) --- (result of attempt)***
 
-// console.log(result.data); undefined
-// console.log(result.topics); undefined
-// console.log(result.data.topics); TypeError
-// console.log(result.data["topics"]);TypeError, does not like 'topics'
-// console.log(result[0]);//undefined
-// console.log(result.[1]);//undefined
-// console.log(axios.get("https://lambda-times-api.herokuapp.com/topics")); //Returns a promise
+// console.log(axios.get("https://lambda-times-api.herokuapp.com/topics")); //Change the out the variable to the axios.get --- Returns a promise
+// console.log(result.data); object notation attempt --- undefined
+// console.log(result.topics); object notation attempt --- undefined
+// console.log(result.data.topics); object notation attempt --- TypeError
+// console.log(result.data["topics"]);object notation attempt --- TypeError, does not like 'topics'
+// console.log(result[0]);//tried calling an array item out of desparation --- undefined
+// console.log(result[1]);//tried calling an array item out of desparation --- undefined
+// console.log(Object.keys(result));// try to at least get the promise object keys to display---returns an empty array
+// const topcisArray = ["javascript","bootstrap","technology","jquery","node.js"];// See if I can get any array to display --- displays the array with no issues
+//console.log(Array.from(result)); // convert the promise into an array --- returns an empty array
+// console.log(result.join('-'));// try to use an array method on the obj --- TypeError, of course it doesn't work
+// console.log(Object.getOwnPropertyNames(result));//try to get the key of the key:value pairs of the promise object --- returns an empty string
+// console.log(Object.getOwnPropertyNames(result.data));//try to get the key of the key:value pairs of the promise object featuring object dot notaion --- TypeError
+// console.log(Object.getOwnPropertyNames(result.data.topics));//try to get the key of the key:value pairs of the promise object featuring object dot notaion pt.2 --- TypeError
+
+
+
+
+
 
 const topicsSection = document.querySelector('topics');
 // debugger
 
-// result.forEach((item) => {
-//     axios
-//     .get("https://lambda-times-api.herokuapp.com/topics");
-//     .then(res => {
-//         console.log(res)
-//         const topicData = res.data.topics;
-//         const newTopic = document.createElement('div')
-//         newTopic.classList.add('tab');
-//         newTopic.textContent = topicData;
-//         topicsSection.appendChild(newTopic);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-// });
+result.forEach((item) => {
+    axios
+    .get("https://lambda-times-api.herokuapp.com/topics");
+    .then(res => {
+        console.log(res)
+        const topicData = res.data.topics;
+        const newTopic = document.createElement('div')
+        newTopic.classList.add('tab');
+        newTopic.textContent = item;
+        topicsSection.appendChild(newTopic);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+});
 
 
 // Attempt at putting the forEach loop inside axios 
